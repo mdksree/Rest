d52bc28231f42d27f64480e388569b778d8c7516
12951-PC\Sreedhar <menaka.srqa@gmail.com>
2018-02-26 15:09:37 -0600

    rest

---
+++ src/main/java/com/qa/client/Login.java
@@ -0,0 +1,19 @@
+package com.qa.client;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Sreedhar\\Desktop\\chromedriver.exe");
+	WebDriver driver =new ChromeDriver();
+	
+		
+		
+		
+	}
+
+}
